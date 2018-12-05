67b1192ebcc93cc4bb794d3bb7d6f084d71b6f8e
Trevor Nodland <trevor.nodland@gmail.com>
2018-12-05 09:17:13 -0700

    Added test for differing ids

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -6,6 +6,7 @@ class JokeTest < Minitest::Test
 
   def setup
     @joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @new_joke = Joke.new(2, "Why did the chicken cross the road?", "I don't know, that's why I was asking you!")
   end
 
   def test_joke_exists
@@ -14,5 +15,9 @@ class JokeTest < Minitest::Test
 
   def test_joke_has_id
     assert_equal 1, @joke.id
+  end
+
+  def test_joke_has_different_id
+    assert_equal 2, @new_joke.id
   end
 end
